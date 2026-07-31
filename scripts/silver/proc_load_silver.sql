b4c2eac334b4de44ee251c09ec74665ccda3887a
GameWindow <147246990+GameWindow@users.noreply.github.com>
2026-07-31 23:23:13 +0400

    Create proc_load_silver.sql

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,51 @@
+USE DataWarehouse;
+GO
+TRUNCATE TABLE silver.crm_cust_info;
+
+INSERT INTO DataWarehouse.silver.crm_cust_info
+(
+cst_id,
+cst_key,
+cst_firstname,
+cst_lastname,
+cst_marital_status,
+cst_gndr,
+cst_create_date
+)
+
+SELECT
+	cst_info.cst_id,
+	cst_info.cst_key,
+	TRIM(cst_info.cst_firstname) cst_firstname ,
+	TRIM(cst_info.cst_lastname) cst_lastname,
+	CASE WHEN UPPER(TRIM(cst_info.cst_marital_status))='S' THEN 'Single'
+		 WHEN UPPER(TRIM(cst_info.cst_marital_status))='M' THEN 'Merried'
+		 ELSE 'n/a'
+		 END cst_marital_status,
+	
+	CASE WHEN UPPER(TRIM(cst_info.cst_gndr))='F' THEN 'Female'
+		 WHEN UPPER(TRIM(cst_info.cst_gndr))='M' THEN 'Male'
+		 ELSE 'n/a'
+		 END cst_gndr,
+	cst_info.cst_create_date
+FROM(
+	SELECT
+		*,
+		ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY	cst_create_date DESC) flag 
+	FROM
+		DataWarehouse.bronze.crm_cust_info
+) cst_info
+WHERE flag=1;
+
+SELECT 
+	prd_id,
+    prd_key,
+    REPLACE(SUBSTRING(prd_info.prd_key,1,5),'-','_')  cat_id,
+	REPLACE(SUBSTRING(prd_info.prd_key,7,LEN(prd_info.prd_key)-7),'-','_')  cat_id,
+	prd_nm,
+    ISNULL(prd_cost,0) prd_cost,
+    ISNULL(prd_line,'n/a'),
+	prd_end_dt prd_start_dt,
+	prd_start_dt prd_end_dt
+    
+  FROM DataWarehouse.bronze.crm_prd_info prd_info
