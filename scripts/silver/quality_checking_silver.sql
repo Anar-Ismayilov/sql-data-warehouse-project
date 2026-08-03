e91f9a18e6d206cfc84761c50c42db46a52f4491
GameWindow <147246990+GameWindow@users.noreply.github.com>
2026-08-03 17:27:04 +0400

    Refactor SQL quality checks for consistency

--- scripts/silver/quality_checking_silver.sql
+++ scripts/silver/quality_checking_silver.sql
@@ -49,14 +49,14 @@ SELECT
 FROM
 	DataWarehouse.silver.crm_prd_info prd_info
 GROUP BY prd_info.prd_id
-HAVING COUNT(prd_info.prd_id)>1 or prd_info.prd_id IS NULL
+HAVING COUNT(prd_info.prd_id)>1 or prd_info.prd_id IS NULL;
 
 --Check for unwanted spaces
 --Expectation: No Result
 select prd_nm
 from
 	DataWarehouse.silver.crm_prd_info
-where TRIM(prd_nm)!=prd_nm
+where TRIM(prd_nm)!=prd_nm;
 
 --Check of Invalid Date Orders
 --Expectation: No Result	
@@ -65,7 +65,7 @@ SELECT
 FROM
 	DataWarehouse.silver.crm_prd_info prd_info
 WHERE
-	prd_info.prd_end_dt<prd_info.prd_start_dt
+	prd_info.prd_end_dt<prd_info.prd_start_dt;
 
 --Data Standardization and Consistency 
 SELECT 
@@ -93,3 +93,78 @@ FROM
 	DataWarehouse.silver.crm_sales_details sal_det
 WHERE 
 	TRIM(sal_det.sls_ord_num) != sal_det.sls_ord_num;
+
+--Check Data Matching
+--Expectation: No Result
+SELECT
+	*
+FROM
+	DataWarehouse.silver.crm_sales_details sal_det
+WHERE 
+	sal_det.sls_prd_key NOT IN (SELECT prd_info.prd_key FROM DataWarehouse.silver.crm_prd_info prd_info);
+
+SELECT
+	*
+FROM
+	DataWarehouse.silver.crm_sales_details sal_det
+WHERE 
+	sal_det.sls_cust_id NOT IN (SELECT cust_info.cst_id FROM DataWarehouse.silver.crm_cust_info cust_info);
+
+--Find Into Bronze for Invalide Date
+
+SELECT
+*
+FROM
+	DataWarehouse.bronze.crm_sales_details sal_det
+WHERE 
+	   sal_det.sls_order_dt<=0 
+	OR LEN(sal_det.sls_order_dt) != 8
+	OR sal_det.sls_order_dt >20500101
+	OR sal_det.sls_order_dt <19000101;
+
+SELECT
+*
+FROM
+	DataWarehouse.bronze.crm_sales_details sal_det
+WHERE 
+	   sal_det.sls_ship_dt<=0 
+	OR LEN(sal_det.sls_ship_dt) != 8
+	OR sal_det.sls_ship_dt >20500101
+	OR sal_det.sls_ship_dt <19000101;
+
+SELECT
+*
+FROM
+	DataWarehouse.bronze.crm_sales_details sal_det
+WHERE 
+	   sal_det.sls_due_dt<=0 
+	OR LEN(sal_det.sls_due_dt) != 8
+	OR sal_det.sls_due_dt >20500101
+	OR sal_det.sls_due_dt <19000101;
+
+--Check Invalide Values
+--Expectation: No Result
+SELECT 
+	sal_det.sls_sales,
+	sal_det.sls_quantity,
+	sal_det.sls_price
+FROM
+	DataWarehouse.silver.crm_sales_details sal_det
+WHERE 
+	sal_det.sls_sales<=0
+	OR sal_det.sls_sales IS NULL
+	OR sal_det.sls_quantity <=0
+	OR sal_det.sls_quantity IS NULL
+	OR sal_det.sls_price <=0
+	OR sal_det.sls_price IS NULL
+	OR sal_det.sls_sales != sal_det.sls_quantity * sal_det.sls_price;
+
+
+=================================================================================================================
+
+	
+	
+
+
+
+
