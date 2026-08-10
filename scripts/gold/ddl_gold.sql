ab32395571afa90cc7cccfc9664092f7cec7e344
Anar Ismayilov <147246990+Anar-Ismayilov@users.noreply.github.com>
2026-08-10 18:49:31 +0400

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,5 +1,27 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- Create Dimension: gold.dim_customers
 USE DataWarehouse;
 GO
+
+IF OBJECT_ID('gold.dim_customers ','V') IS NOT NULL
+	DROP VIEW gold.dim_customers ;
+GO
+
 CREATE VIEW gold.dim_customers AS 
 SELECT
 	ROW_NUMBER() OVER (ORDER BY ci.cst_id ) AS customer_key, 
@@ -23,7 +45,12 @@ ON ci.cst_key = ca.cid
 LEFT JOIN DataWarehouse.silver.erp_loc_a101 lo
 ON ci.cst_key = lo.cid;
 
+GO
+IF OBJECT_ID('gold.dim_products ','V') IS NOT NULL
+	DROP VIEW gold.dim_products ;
+GO
 
+-- Create Dimension: gold.dim_products
 CREATE VIEW gold.dim_products AS
 SELECT
 	ROW_NUMBER() OVER (ORDER BY pi.prd_start_dt, pi.prd_key) prd_key,
@@ -41,5 +68,30 @@ FROM
 	DataWarehouse.silver.crm_prd_info pi
 LEFT JOIN DataWarehouse.silver.erp_px_cat_g1v2 pc
 ON pi.cat_id = pc.id
-WHERE pi.prd_end_dt IS NULL
+WHERE pi.prd_end_dt IS NULL;
+GO
 
+
+-- Create Fact Table: gold.fact_sales
+
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  AS order_number,
+    pr.prd_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+    ON sd.sls_cust_id = cu.customer_id;
+GO
